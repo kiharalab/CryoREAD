8224a7820c1389c832f5a3978efc247140152b2f
Xiao Wang <wang3702@purdue.edu>
2023-10-04 00:17:34 -0400

    fix small fasta bug

--- ops/fasta_utils.py
+++ ops/fasta_utils.py
@@ -22,7 +22,7 @@ def read_fasta(input_fasta_path,dna_check=False):
                 if "_" in current_id:
                     current_id = current_id.split("_")[1]
                 read_chain=True
-            elif len(line)>0:
+            elif len(line.strip("\n").replace(" ",""))>0:
                 if current_id is None or read_chain==False or len(current_id)!=1:
                     visit_set=list(chain_dict.keys())
                     for tmp_chain in tmp_chain_list:
